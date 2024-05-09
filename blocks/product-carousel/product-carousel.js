8816ef827ebad1c15cee5468395a82d1ab6eefc6
Sean Steimer <ssteimer@adobe.com>
2024-05-09 10:22:09 -0700

    chore: product carousel prices and reviews

--- blocks/product-carousel/product-carousel.js
+++ blocks/product-carousel/product-carousel.js
@@ -2,6 +2,7 @@ import {
   decorateBlock,
   loadBlock,
   buildBlock,
+  toClassName,
 } from '../../scripts/aem.js';
 import { createSlide } from '../carousel/carousel.js';
 
@@ -9,7 +10,6 @@ async function loadSlide(slide) {
   const resp = await fetch(`https://www.macys.com/xapi/digital/v1/product/${slide.dataset.productId}`);
   if (resp.ok) {
     const json = await resp.json();
-    console.log(json);
 
     const slideRow = document.createElement('div');
     const imageCol = document.createElement('div');
@@ -27,10 +27,44 @@ async function loadSlide(slide) {
     const contentCol = document.createElement('div');
     contentCol.innerHTML = `
       <a class="product-link" title="${product.detail.name}" href="https://www.macys.com${product.identifier.productUrl}">
-      <span>${product.detail.brand.name}</span>
+        <span>${product.detail.brand.name}</span>
         <h3>${product.detail.name}</h3>
       </a>
     `;
+    const productPricing = document.createElement('div');
+    productPricing.className = 'product-pricing';
+    product.pricing.price.tieredPrice.forEach((price) => {
+      const { label, values } = price;
+      const formattedLabel = label.replace('[PRICE]', values[0].formattedValue);
+      const priceHolder = document.createElement('div');
+      priceHolder.className = 'price-holder';
+      priceHolder.innerHTML = `<p class="price tiered-price price-${toClassName(values[0].type)}">${formattedLabel}</p>`;
+      productPricing.append(priceHolder);
+    });
+    if (product.pricing.price.finalPrice) {
+      const priceHolder = document.createElement('div');
+      priceHolder.className = 'price-holder';
+      priceHolder.innerHTML = `<p class="price price-final">${product.pricing.price.finalPrice.formattedFinalPrice}</p>`;
+      if (product.pricing.price.finalPrice.applicablePromotions) {
+        product.pricing.price.finalPrice.applicablePromotions.forEach((promoCode) => {
+          const { header } = product.pricing.badgesMap[promoCode];
+          const priceHeader = document.createElement('p');
+          priceHeader.className = 'price-header';
+          priceHeader.textContent = header;
+          priceHolder.prepend(priceHeader);
+        });
+      }
+      productPricing.append(priceHolder);
+    }
+    contentCol.append(productPricing);
+
+    if (product.detail.reviewStatistics.aggregate) {
+      const reviews = document.createElement('div');
+      reviews.className = 'product-reviews';
+      reviews.innerHTML = `<span class="review-stars" style="--rating-pct: ${product.detail.reviewStatistics.aggregate.ratingPercentage}%;"></span><span> (${product.detail.reviewStatistics.aggregate.count})</span>`;
+      contentCol.append(reviews);
+    }
+
     slideRow.append(imageCol, contentCol);
     slide.replaceChildren(...createSlide(slideRow, slide.dataset.slideIndex, 0).children);
   }
@@ -43,6 +77,7 @@ async function loadSlide(slide) {
  */
 export default async function decorate(block) {
   const productIds = [];
+  const additionalContent = [];
   block.querySelectorAll(':scope > div').forEach((row, rowIndex) => {
     if (rowIndex === 0) {
       const list = row.querySelector('ul');
@@ -56,7 +91,14 @@ export default async function decorate(block) {
         });
       }
     } else {
-      // extra content, TODO
+      row.className = 'additional-content';
+      row.querySelectorAll('.button-container').forEach((btnCon) => {
+        btnCon.classList.remove('button-container');
+        btnCon.querySelectorAll('.button').forEach((btn) => {
+          btn.classList.remove('button');
+        });
+      });
+      additionalContent.push(row);
     }
   });
   const carouselContent = [];
@@ -68,6 +110,9 @@ export default async function decorate(block) {
   });
   const carousel = buildBlock('carousel', carouselContent);
   block.replaceChildren(carousel);
+  if (additionalContent.length > 0) {
+    block.prepend(...additionalContent);
+  }
   decorateBlock(carousel);
   await loadBlock(carousel);
 
