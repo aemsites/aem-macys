14398708c83fd824990d6fad2023e179ef617288
Sean Steimer <ssteimer@adobe.com>
2024-05-10 16:41:42 -0700

    mobile header visible styling

--- blocks/header-mobile/header-mobile.js
+++ blocks/header-mobile/header-mobile.js
@@ -1,5 +1,45 @@
 import { loadFragment } from '../fragment/fragment.js';
-import { div, nav, domEl } from '../../scripts/dom-helpers.js';
+import {
+  div, nav, button, span, domEl
+} from '../../scripts/dom-helpers.js';
+
+function decorateBrand(brandSection) {
+  if (!brandSection) return;
+
+  const btnCont = brandSection.querySelector('.button-container');
+  const btn = btnCont.querySelector('.button');
+  btnCont.before(btn);
+  btnCont.remove();
+  btn.classList.remove('button');
+}
+
+function decorateSearch(searchSection) {
+  if (!searchSection) return;
+
+  const icon = searchSection.querySelector('.icon');
+  searchSection.replaceChildren(div(
+    { class: 'search-input-container' },
+    icon,
+    domEl('input', {
+      class: 'search-input',
+      'aria-description': 'search',
+      placeholder: 'Search',
+      type: 'text',
+    }),
+  ));
+}
+
+function decorateTools(toolsSection) {
+  if (!toolsSection) return;
+
+  toolsSection.querySelectorAll('a').forEach((a) => {
+    const hasIcon = a.querySelector('.icon');
+    if (!hasIcon) {
+      a.classList.add('button');
+    }
+  });
+}
+
 /**
  * loads and decorates the footer
  * @param {Element} block The footer block element
@@ -23,8 +63,18 @@ export default async function decorate(block) {
       upper.append(section);
     }
   });
-  const hamburger = div();
+  const hamburger = div(
+    { class: 'nav-hamburger' },
+    button({
+      type: 'button',
+      'aria-controls': 'mobile-nav',
+      'aria-label': 'Open Navigation',
+    }, span({ class: 'nav-hamburger-icon' })),
+  );
   upper.prepend(hamburger);
+  decorateBrand(upper.querySelector('.section.brand'));
+  decorateTools(upper.querySelector('.section.tools'));
+  decorateSearch(upper.querySelector('.section.search'));
 
   ['user', 'sections', 'utility'].forEach((sectionName) => {
     const section = headerMain.querySelector(`.section.${sectionName}`);
