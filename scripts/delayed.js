c8e85d3608cec997e63b04c210599894be83e98f
Sean Steimer <ssteimer@adobe.com>
2024-05-21 10:32:58 -0700

    add helper to flush fragment cache

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -1,7 +1,25 @@
 // eslint-disable-next-line import/no-cycle
 import { sampleRUM } from './aem.js';
 
-// Core Web Vitals RUM collection
-sampleRUM('cwv');
+/**
+ * a helper to flush fragment plain html from browser cache so when authors edit them
+ * they get flushed so when they subsequently view referencing pages
+ * they see the right result
+ */
+async function flushFragmentCache() {
+  const isFragment = ['/fragments/', '/nav-menus/', '/footer', '/nav-mobile', '/nav-desktop']
+    .some((fragmentPath) => window.location.pathname.includes(fragmentPath));
+  if (isFragment) {
+    fetch(`${window.location.pathname}.plain.html`, { cache: 'reload' });
+  }
+}
 
-// add more delayed functionality here
+async function runDelayed() {
+  // Core Web Vitals RUM collection
+  sampleRUM('cwv');
+
+  // add more delayed functionality here
+  flushFragmentCache();
+}
+
+runDelayed();
