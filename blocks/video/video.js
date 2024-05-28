4425e834567e5f3d1e7598f24203b934fec842fc
Sean Steimer <ssteimer@adobe.com>
2024-05-28 16:27:41 -0700

    video pasue control

--- blocks/video/video.js
+++ blocks/video/video.js
@@ -1,5 +1,7 @@
 import { removeButtons } from '../../scripts/scripts.js';
 
+const prefersReducedMotion = window.matchMedia('(prefers-reduced-motion: reduce)');
+
 const loadVideo = (container, autoplay) => {
   const video = document.createElement('video');
   video.setAttribute('controls', '');
@@ -7,13 +9,17 @@ const loadVideo = (container, autoplay) => {
   video.addEventListener('loadedmetadata', () => delete video.dataset.loading);
 
   if (autoplay) {
-    video.setAttribute('autoplay', '');
+    if (!prefersReducedMotion.matches) {
+      video.setAttribute('autoplay', '');
+    }
     video.setAttribute('loop', '');
     video.setAttribute('playsinline', '');
     video.removeAttribute('controls');
     video.addEventListener('canplay', () => {
       video.muted = true;
-      video.play();
+      if (!prefersReducedMotion.matches) {
+        video.play();
+      }
     });
   }
 
@@ -45,9 +51,34 @@ export default async function decorate(block) {
 
   const overlay = document.createElement('div');
   overlay.className = 'video-content-overlay';
+
+  const togglePlayState = document.createElement('button');
+  togglePlayState.setAttribute('type', 'button');
+  togglePlayState.setAttribute('aria-label', 'Pause Video');
+  togglePlayState.className = 'video-control';
+  togglePlayState.addEventListener('click', () => {
+    togglePlayState.classList.toggle('playing');
+    const vid = block.querySelector('video');
+    if (vid) {
+      if (vid.paused) {
+        vid.play();
+        togglePlayState.setAttribute('aria-label', 'Pause Video');
+      } else {
+        vid.pause();
+        togglePlayState.setAttribute('aria-label', 'Play Video');
+      }
+    }
+  });
+  if (!prefersReducedMotion.matches) {
+    togglePlayState.classList.add('playing');
+    togglePlayState.setAttribute('aria-label', 'Pause Video');
+  }
+  overlay.append(togglePlayState);
+
   const overlayLink = block.querySelector('.video-content a');
   if (overlayLink) {
     const cloned = overlayLink.cloneNode(false);
+    cloned.classList.add('shop-link');
     overlay.append(cloned);
   }
   content.push(overlay);
