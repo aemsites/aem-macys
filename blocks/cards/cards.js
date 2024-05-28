cdbd8b4d9f6b68a22d245f75221a8d1d51c61ae7
Sean Steimer <ssteimer@adobe.com>
2024-05-28 12:02:23 -0700

    chore: cards

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -1,5 +1,3 @@
-import { createOptimizedPicture } from '../../scripts/aem.js';
-
 export default function decorate(block) {
   /* change to ul, li */
   const ul = document.createElement('ul');
@@ -7,12 +5,19 @@ export default function decorate(block) {
     const li = document.createElement('li');
     while (row.firstElementChild) li.append(row.firstElementChild);
     [...li.children].forEach((div) => {
-      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
-      else div.className = 'cards-card-body';
+      if (div.children.length === 1 && div.querySelector('picture')) {
+        div.className = 'cards-card-image';
+      } else {
+        div.className = 'cards-card-body';
+        div.querySelectorAll('.button-container').forEach((btnCon) => {
+          btnCon.classList.remove('button-container');
+          btnCon.querySelectorAll('.button').forEach((btn) => {
+            btn.classList.remove('button');
+          });
+        });
+      }
     });
     ul.append(li);
   });
-  ul.querySelectorAll('img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
-  block.textContent = '';
-  block.append(ul);
+  block.replaceChildren(ul);
 }
